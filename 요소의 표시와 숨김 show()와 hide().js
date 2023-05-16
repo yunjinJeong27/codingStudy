12cae8a6374141e076279694c5be395ad29133f3
yunjinJeong27 <125287003+yunjinJeong27@users.noreply.github.com>
2023-05-16 15:43:12 +0900

    요소의 표시와 숨김 show()와 hide()

--- 요소의 표시와 숨김 show()와 hide().js
+++ 요소의 표시와 숨김 show()와 hide().js
@@ -1,8 +1,8 @@
 $(function() {
     $("#showBtn").on("click", function() {
-        $("#example").show(); // id가 "example"인 요소를 나타나게 함.
+        $("#example").show(3000);   // id가 "example"인 요소를 3초에 걸쳐 나타나게 함.
     });
     $("#hideBtn").on("click", function() {
-        $("#example").hide(); // id가 "example"인 요소를 숨김.
+        $("#example").hide("fast"); // id가 "example"인 요소를 빠르게 숨김.
     });
 });
