4e31a9f44ffa29546faaea0535665ae41e30a0cc
yunjinJeong27 <125287003+yunjinJeong27@users.noreply.github.com>
2023-06-30 10:16:32 +0900

    Java 산술 연산자(arithmetic operator)

---
+++ Java 산술 연산자(arithmetic operator).java
@@ -0,0 +1,15 @@
+int num1 = 8, num2 = 4;
+
+System.out.println("+ 연산자에 의한 결과 : "+ (num1 + num2));
+System.out.println("- 연산자에 의한 결과 : "+ (num1 - num2));
+
+System.out.println("* 연산자에 의한 결과 : "+ (num1 * num2));
+
+System.out.println("/ 연산자에 의한 결과 : "+ (num1 / num2));
+System.out.println("% 연산자에 의한 결과 : "+ (num1 % num2));
+
+// + 연산자에 의한 결과 : 12
+// - 연산자에 의한 결과 : 4
+// * 연산자에 의한 결과 : 32
+// / 연산자에 의한 결과 : 2
+// % 연산자에 의한 결과 : 0
