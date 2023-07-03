9dbccf941ab6b6e1575e104a76e6b9594fd337b5
yunjinJeong27 <125287003+yunjinJeong27@users.noreply.github.com>
2023-07-03 10:10:48 +0900

    Java 대입 연산자(assignment operator)

---
+++ Java 대입 연산자(assignment operator).java
@@ -0,0 +1,13 @@
+int num1 = 7, num2 = 7, num3 = 7;
+
+num1 = num1 - 3;
+num2 -= 3;
+num3 =- 3;
+
+System.out.println("- 연산자에 의한 결과 : "+ num1);
+System.out.println("-= 연산자에 의한 결과 : "+ num2);
+System.out.println("=- 연산자에 의한 결과 : "+ num3);
+
+// -  연산자에 의한 결과 : 4
+// -= 연산자에 의한 결과 : 4
+// =- 연산자에 의한 결과 : -3
