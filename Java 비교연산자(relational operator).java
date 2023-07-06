c5373836b629813ad3f0bb862e7bb7cbb4a7c6c3
yunjinJeong27 <125287003+yunjinJeong27@users.noreply.github.com>
2023-07-06 09:43:10 +0900

    Java 비교연산자(relational operator)

---
+++ Java 비교연산자(relational operator).java
@@ -0,0 +1,8 @@
+char ch1 = 'a', ch2 = 'A';
+
+System.out.println("== 연산자에 의한 결과 : "+ (ch1 == ch2));
+// 실행결과: == 연산자에 의한 결과 : false
+
+System.out.println("> 연산자에 의한 결과 : "+ (ch1 > ch2));
+// 실행결과: >  연산자에 의한 결과 : true
+// 아스키코드에서 'A'는 10진수 65, 'a'는 10진수 97
