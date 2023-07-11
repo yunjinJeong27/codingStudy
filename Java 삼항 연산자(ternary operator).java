461dff195abc0290d820e1bca6607ad0b8c838c9
yunjinJeong27 <125287003+yunjinJeong27@users.noreply.github.com>
2023-07-11 16:06:13 +0900

    Java 삼항 연산자(ternary operator)

---
+++ Java 삼항 연산자(ternary operator).java
@@ -0,0 +1,7 @@
+// 삼항 연산자 - 조건식 ? 반환값1 : 반환값2
+int num1 = 5, num2 = 7;
+int result;
+
+result = (num1 - num2 > 0) ? num1 : num2;
+System.out.println("두 정수 중 더 큰 수는 " + result + "입니다.");
+// 두 정수 중 더 큰 수는 7입니다.
