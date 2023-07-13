2c34f9527221f1bafe5c5efa5ad5446f8a16aac3
yunjinJeong27 <125287003+yunjinJeong27@users.noreply.github.com>
2023-07-13 09:56:59 +0900

    Java if 문

---
+++ Java if 문.java
@@ -0,0 +1,19 @@
+if (ch >= 'a' && ch <= 'z') {
+    System.out.println("해당 문자는 영문 소문자입니다.");
+} //해당 문자는 영문 소문자입니다.
+
+
+if (ch >= 'a' && ch <= 'z') {
+    System.out.println("해당 문자는 영문 소문자입니다.");
+} else {
+    System.out.println("해당 문자는 영문 소문자가 아닙니다.");
+} //해당 문자는 영문 소문자가 아닙니다.
+
+
+if (ch >= 'a' && ch <= 'z') {
+    System.out.println("해당 문자는 영문 소문자입니다.");
+} else if (ch >= 'A' && ch <= 'Z') {
+    System.out.println("해당 문자는 영문 대문자입니다.");
+} else {
+    System.out.println("해당 문자는 영문자가 아닙니다.");
+} //해당 문자는 영문 대문자입니다.
