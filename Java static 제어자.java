1dd45d3e4da726f3336fb8024a0d5e73df1f88ea
yunjinJeong27 <125287003+yunjinJeong27@users.noreply.github.com>
2023-08-22 17:45:00 +0900

    Java static 제어자

---
+++ Java static 제어자.java
@@ -0,0 +1,9 @@
+class Car {
+    static int var;       // 클래스 필드(static 변수)
+    static {              // static 초기화 블록
+    // 보통 클래스 필드의 초기화를 진행함.
+    {
+    static void brake() { // 클래스 메소드(static 메소드)
+         ...
+    }
+}
